169b872e0109131c3cacacf725d0888730863e1f
marcoxue814 <60197340+marcoxue814@users.noreply.github.com>
2022-10-05 21:40:21 -0700

    Add files via upload

---
+++ var.rkhs.surv.R
@@ -0,0 +1,32 @@
+var.rkhs.survival <- function(times, failures, variable, weights=NULL)
+{
+  if(sum(times<0)>0){print("Error times must be positive")
+  }else{
+    if(sum(weights<=0)>0){print("Error weights must be superior to 0")
+    }else{
+      if(sum(failures!=0 & failures!=1)>0){print("Error failures must be a vector of 0 or 1")
+      }else{
+        if(is.null(weights)){ .w <- rep(1, length(times)) }	else { .w <- weights }
+        .data <- data.frame(t=times, f=failures, v=variable, w=.w)
+        .data <- .data[!is.na(.data$v),]
+        table.surv <- data.frame(times=NULL, n.risk=NULL, n.event=NULL, survival=NULL, variance = NULL, variable=NULL)
+        for(i in  unique(variable)){
+          .d <- .data[.data$v==i,]
+          .tj <- c(0,sort(unique(.d$t[.d$f==1])),max(.d$t))
+          .dj <- sapply(.tj, function(x){sum(.d$w[.d$t==x & .d$f==1])})
+          .nj <- sapply(.tj, function(x){sum(.d$w[.d$t>=x])})
+          .st <- cumprod((.nj-.dj)/.nj)
+          .mj <- sapply(.tj, function(x){(sum(.d$w[.d$t>=x]))^2/sum((.d$w[.d$t>=x])^2)})
+          .vt <- (.st)^2*cumsum(.dj/(.mj * (.nj - .dj)))
+          table.surv <- rbind(table.surv, data.frame(times = .tj, 
+                                                     n.risk = .nj, n.event = .dj, survival = .st,
+                                                     variance = .vt,
+                                                     variable = i))
+        }
+        .obj <- list(table.surv=table.surv)
+        class(.obj) <- "survrisca"
+        return(.obj)
+      }
+    }
+  }
+}
